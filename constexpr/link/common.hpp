29d570cf5d3817b146702200ffdab200cf202ea8
Aurelian MELINTE <ame01@gmx.net>
2025-04-25 09:09:45 -0400

    common.hpp

--- constexpr/link/common.hpp
+++ constexpr/link/common.hpp
@@ -1,6 +1,14 @@
 // --- common.hpp
 #pragma once
+
+/*
+$ nm -C a.out | grep _c
+36:0000000000002004 r _c
+37:0000000000002008 r _c
+38:000000000000200c r _c
+*/
 constexpr int _c = 42;
+
 extern const int* func1();
 extern const int* func2();
 
