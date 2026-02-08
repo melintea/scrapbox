192d5cf777344ed42539351ea43ad79d17795bd5
Aurelian MELINTE <ame01@gmx.net>
2026-02-08 11:48:57 -0500

    Embeddign pretty printers in binaries

---
+++ embed-gdb-pretty-printer/main.cpp
@@ -0,0 +1,50 @@
+//
+// https://github.com/epasveer/seer/wiki/Gdb%27s-Pretty-Print-feature
+//
+
+#include "gdb-ppe.h" // LocationPrinter gdb pretty printer
+
+#include <string>
+
+struct Location {
+    std::string      city;
+    std::string      state;
+    int              zip;
+    unsigned int     cell;
+};
+
+struct Person {
+    std::string      name;
+    int              age;
+    float            salary;
+    struct Location  location;
+};
+
+void test(const Person& p)
+{
+    auto p2 = p;
+}
+
+
+int main (int argc, char** argv) {
+
+    Location where;
+    where.city         = "Houston";
+    where.state        = "Texas";
+    where.zip          = 77063;
+    where.cell         = 2226660000;
+
+
+    Person me;
+    me.name            = "Pasveer, Ernie";
+    me.age             = 60;
+    me.salary          = 0.25;
+    me.location.city   = "Houston";
+    me.location.state  = "Texas";
+    me.location.zip    = 77063;
+    me.location.cell   = 2226669999;
+    test(me);
+
+    return 0;
+}
+
