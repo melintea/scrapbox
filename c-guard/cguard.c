a512880bd3c55bc754bef8091c84cb6973859bb4
Aurelian MELINTE <ame01@gmx.net>
2026-08-17 11:25:58 -0400

    C guards

--- c-guard/cguard.c
+++ c-guard/cguard.c
@@ -3,10 +3,26 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-static void int_cleanup(int* px)
+static inline void int_cleanup(int* px)
 {
     printf("int_cleanup: %d\n", *px);
+}
+
+static inline void mem_cleanup(void* pp)
+{
+    void* p = *((void**)pp);
+    printf("mem_cleanup: %p\n", p);
+    free(p);
+}
+
+static inline void fopen_cleanup(void* pp)
+{
+    FILE* p = *((FILE**)pp);
+    printf("fopen_cleanup: %p\n", p);
+    if (p) { fclose(p); };
 }
 
 int main()
@@ -14,6 +30,16 @@ int main()
     {
         int x __attribute__((cleanup(int_cleanup))) = 42;
     }
+    {
+        char* p __attribute__((cleanup(mem_cleanup))) = strdup("some str");
+    }
+    {
+        void* p __attribute__((cleanup(mem_cleanup))) = malloc(1023);
+    }
+    {
+        FILE* pf __attribute__((cleanup(fopen_cleanup))) = fopen("/no/such/file", "r");
+    }
+    
     printf("main: done\n");
     return 0;
 }
